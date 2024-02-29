07ac0553550e20be08fe5811893a8148b18d6652
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-02-29 21:20:35 +0700

    Update Time.java

--- src/main/java/com/mycompany/app/Time.java
+++ src/main/java/com/mycompany/app/Time.java
@@ -1,1 +1,9 @@
+public class Time {
+ int hour;
+ int minute;
+ int second;
+ Time() { setTime(0, 0, 0); }
+ Time(int h) { setTime(h, 0, 0); }
+ Time(int h, int m) { setTime(h, m, 0); }
+ Time(int h, int m, int s) { setTime(h, m, s); }
 
