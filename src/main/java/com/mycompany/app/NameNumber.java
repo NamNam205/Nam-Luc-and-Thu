5de84cc0aaf32df6b2eaae2967c0ac6c30807d91
NamNam205 <namqtnh134@gmail.com>
2024-03-01 08:23:35 +0700

    k

--- src/main/java/com/mycompany/app/NameNumber.java
+++ src/main/java/com/mycompany/app/NameNumber.java
@@ -1,1 +1,2 @@
+package com.mycompany.app;
 
--- src/main/java/com/mycompany/app/Week1/Assignment2.java
+++ src/main/java/com/mycompany/app/Week1/Assignment2.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.mycompany.app.Week1;
 
 public class Assignment2 {
     public static void main(String[] args) {
