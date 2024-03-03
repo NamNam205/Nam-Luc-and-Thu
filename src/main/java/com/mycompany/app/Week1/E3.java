8a01d9ad6c37e91f9d082d11904e9264ef88ac92
NamNam205 <namqtnh134@gmail.com>
2024-03-03 17:43:41 +0700

    3/3

---
+++ src/main/java/com/mycompany/app/Week1/E3.java
@@ -0,0 +1,9 @@
+package com.mycompany.app.Week1;
+
+public class E3 {
+    public void Ex3() {
+        while (true) {
+
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/app/Week1/E5.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.Week1;
+
+public class E5 {
+    public void Ex5() {
+
+    }
+}
