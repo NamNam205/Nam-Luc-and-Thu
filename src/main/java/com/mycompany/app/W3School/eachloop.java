7f790d66f0db7d0c0fe0f9bab15ca10fb18c8091
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-04 11:15:53 +0700

    Update eachloop.java

--- src/main/java/com/mycompany/app/W3School/eachloop.java
+++ src/main/java/com/mycompany/app/W3School/eachloop.java
@@ -1,8 +1,8 @@
 public class eachloop {
-	public static void main(String[] args) {
-String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-for (String i : cars) {
-  System.out.println(i);
-}
+  public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+      System.out.println(i);
+    }    
   }
 }
