85e9dd00aba1501cdd32336afa549d133f65b34e
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-04 11:28:56 +0700

    Update switchcase.java

--- src/main/java/com/mycompany/app/W3School/switchcase.java
+++ src/main/java/com/mycompany/app/W3School/switchcase.java
@@ -1,1 +1,15 @@
-pulic class switchcase.java
+package com.mycompany.app.W3School;
+public class whileloop {
+	public static void main(String[] args) {
+    int day = 2;
+switch (day) {
+  case 1:
+    System.out.println("Saturday");
+    break;
+  case 2:
+    System.out.println("Sunday"); 
+break;
+}
+  }
+}
+
