66d89b7a265aa3a57c023cae57058a5e313caf9f
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-04 11:01:31 +0700

    Update ifelse.java

--- src/main/java/com/mycompany/app/W3School/ifelse.java
+++ src/main/java/com/mycompany/app/W3School/ifelse.java
@@ -1,5 +1,19 @@
 package com.mycompany.app.W3School;
+import java.until.Scanner;
 
-public class abc {
-    
+public class ifelse {
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        if (a == b) {
+            System.out.println("a = b");
+        }
+        else if (a > b) {
+            System.out.println("a > b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
 }
