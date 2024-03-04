13f3087d660d5e51cd1a027459977b5261813b7e
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-04 11:42:54 +0700

    Update and rename forloop.java to Eforloop.java

--- src/main/java/com/mycompany/app/W3School/Eforloop.java
+++ src/main/java/com/mycompany/app/W3School/Eforloop.java
@@ -1,5 +1,5 @@
 package com.mycompany.app.W3School;
-public class forloop {
+public class Eforloop {
 	public static void main(String[] args) {
 for (int i = 1; i <= 5; i++){
   System.out.println("yes");
