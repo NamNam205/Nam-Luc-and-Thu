4d6c3f77f710f91df14e5797ba70e71df1878431
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-06 17:04:41 +0700

    Update Ebreak.java

--- src/main/java/com/mycompany/app/W3School/Ebreak.java
+++ src/main/java/com/mycompany/app/W3School/Ebreak.java
@@ -2,7 +2,7 @@ package com.mycompany.app.W3School;
 public class Ebreak {
    public static void main(String[] args)  {
     for (int i = 0; i < 10; i++) {
-  if (i == 4) {
+  if (i = 4) {
     break;
   }
   System.out.println(i);
