76eb5b2e0e92c9f4ec63942ba29cd927a1679086
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-06 10:39:17 +0000

    a

--- src/main/java/com/mycompany/app/W3School/Ewhileloop.java
+++ src/main/java/com/mycompany/app/W3School/Ewhileloop.java
@@ -1,11 +1,12 @@
 package com.mycompany.app.W3School;
+
 public class Ewhileloop {
-	public static void main(String[] args) {
-      int i = 1;
- while (i < 6) {
-  System.out.println(i); 
-  i++;
-}  
-	}
+  public static void main(String[] args) {
+    int i = 1;
+    while (i < 6) {
+      System.out.println(i);
+      i++;
+    }
+  }
 }
-	
+//gggr
