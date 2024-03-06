7c93e3f2c0994b8bdc1834ec157c6cc2578ed643
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-06 10:37:33 +0000

    zz

--- src/main/java/com/mycompany/app/W3School/Eeachloop.java
+++ src/main/java/com/mycompany/app/W3School/Eeachloop.java
@@ -1,9 +1,12 @@
 package com.mycompany.app.W3School;
+
 public class Eeachloop {
   public static void main(String[] args) {
     String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
     for (String i : cars) {
       System.out.println(i);
+      System.out.println(i);
     }    
   }
+  //bnvbvnbv
 }
