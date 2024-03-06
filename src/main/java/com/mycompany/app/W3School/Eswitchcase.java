c011842665b9d0ca02fd4e45823b741319773680
lucnguyenoder <160729893+lucnguyenoder@users.noreply.github.com>
2024-03-06 10:40:31 +0000

    fixed

--- src/main/java/com/mycompany/app/W3School/Eswitchcase.java
+++ src/main/java/com/mycompany/app/W3School/Eswitchcase.java
@@ -1,15 +1,15 @@
 package com.mycompany.app.W3School;
-public class Eswitchcase {
-	public static void main(String[] args) {
-    int day = 2;
-switch (day) {
-  case 1:
-    System.out.println("Saturday");
-    break;
-  case 2:
-    System.out.println("Sunday"); 
-break;
-}
-  }
-}
 
+public class Eswitchcase {
+  public static void main(String[] args) {
+    int day = 2;
+    switch (day) {
+      case 1:
+        System.out.println("Saturday");
+        break;
+      case 2:
+        System.out.println("Sunday");
+        break;
+    }
+  }
+} //ddfdds
