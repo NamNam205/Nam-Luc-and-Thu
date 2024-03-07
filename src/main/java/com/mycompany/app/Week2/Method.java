68b0ab847b03750bd4202f2c1f5767fa4ab02e8f
NamNam205 <160729268+NamNam205@users.noreply.github.com>
2024-03-07 17:18:31 +0700

    Update Method.java

--- src/main/java/com/mycompany/app/Week2/Method.java
+++ src/main/java/com/mycompany/app/Week2/Method.java
@@ -1,11 +1,2 @@
 package com.mycompany.app.Week2;
 
-static void myMethod() {
-  System.out.println("I just got executed!");
-}
-
-public static void main(String[] args) {
-  
-myMethod();
-}
-//nam
