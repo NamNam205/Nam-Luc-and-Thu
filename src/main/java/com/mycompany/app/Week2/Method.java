45b2040038ad8d486d4a123f0f27c0b28c433329
NamNam205 <namqtnh134@gmail.com>
2024-03-08 07:57:22 +0700

    a

--- src/main/java/com/mycompany/app/Week2/Method.java
+++ src/main/java/com/mycompany/app/Week2/Method.java
@@ -1,2 +1,11 @@
 package com.mycompany.app.Week2;
 
+    static void myMethod() {
+      System.out.println("I just got executed!");
+    }
+  
+    public static void main(String[] args) {
+      myMethod();
+      myMethod();
+      myMethod();
+    }
