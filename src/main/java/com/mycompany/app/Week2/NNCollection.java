3aec32a23c5dcfe4840f88bca7443c4314cf9f5e
NamNam205 <160729268+NamNam205@users.noreply.github.com>
2024-03-10 21:30:43 +0700

    Update NNCollection.java

--- src/main/java/com/mycompany/app/Week2/NNCollection.java
+++ src/main/java/com/mycompany/app/Week2/NNCollection.java
@@ -1,21 +1,37 @@
 package com.mycompany.app.Week2;
 import java.util.Scanner;
 public class NNCollection {
- private NameNumber[] nnArray = new NameNumber[100];
- private int free;
- public NNCollection() {free = 0;}
- public void insert(NameNumber n) {
- int index = 0;
- for (int i = free++;i != 0 && nnArray[i-1].getLastName().compareTo(n.getLastName()) > 0;i--) {
- nnArray[i] = nnArray[i-1];
- index = i;
- }
- nnArray[index] = n;
- }
-  public String findNumber(String lName) {
- for (int i = 0; i != free; i++)
- if (nnArray[i].getLastName().equals(lName))
- return nnArray[i].getTelNumber();
- return new String("Name not found");
-  }
-} 
+    private NameNumber[] nnArray;
+    private int free;
+
+    // Constructor
+    public NNCollection() {
+        nnArray = new NameNumber[100];
+        free = 0;
+    }
+
+    // Chèn một đối tượng NameNumber mới vào trong bộ sưu tập
+    public void insert(NameNumber n) {
+        int index = free;
+
+        // Sử dụng sắp xếp chèn để duy trì thứ tự theo thứ tự chữ cái của họ
+        while (index > 0 && nnArray[index - 1].getLastName().compareTo(n.getLastName()) > 0) {
+            nnArray[index] = nnArray[index - 1];
+            index--;
+        }
+
+        nnArray[index] = n;
+        free++;
+    }
+
+    // Tìm số điện thoại liên quan đến một họ đã cho
+    public String findNumber(String lName) {
+        for (int i = 0; i < free; i++) {
+            if (nnArray[i].getLastName().equals(lName)) {
+                return nnArray[i].getTelNumber();
+            }
+        }
+
+        return "Không tìm thấy tên";
+    }
+}
